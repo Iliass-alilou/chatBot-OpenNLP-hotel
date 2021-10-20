7ae407afb10726471ec35033cb23a5436845024c
iliass alilou <iliassalilou18@gmail.com>
2021-10-20 23:24:43 +0100

    htolechatbot openNLP Tokeniz Limetiz POS'

---
+++ src/main/java/com/microservice/ChatBotHotelApplication.java
@@ -0,0 +1,13 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotHotelApplication.class, args);
+	}
+
+}
